19bb59395fd08759c7a1533696f49f30d31a6334
tohdi <tohdi1@gmail.com>
2020-03-30 02:56:00 -0300

    Added 2 new levels.

--- src/LevelMapper.php
+++ src/LevelMapper.php
@@ -17,11 +17,17 @@ class LevelMapper
 
     const LEVEL_ERROR_SERVER      = 5;
 
+    const LEVEL_ERROR_CONSOLE     = 6;
+
+    const LEVEL_ERROR_JOB         = 7;
+
     public static $mapper = [
         1 => [ 'label' => 'Notificação' ],
         2 => [ 'label' => 'Aviso' ],
         3 => [ 'label' => 'Erro: Sistema' ],
         4 => [ 'label' => 'Erro: Persistência' ],
-        5 => [ 'label' => 'Erro: Servidor' ]
+        5 => [ 'label' => 'Erro: Servidor' ],
+        6 => [ 'label' => 'Erro: Console' ],
+        7 => [ 'label' => 'Erro: Execução de Job' ],
     ];
 }
